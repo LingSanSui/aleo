fa8f5767731ceda4dce1188bad0a4c60346e119f
simplefish0909 <116713505+simplefish0909@users.noreply.github.com>
2022-11-15 15:06:52 +0800

    Update aleo_simplefish.sh
    优化

--- aleo_simplefish.sh
+++ aleo_simplefish.sh
@@ -1,27 +1,30 @@
-Workspace=/aleo/snarkOS
+#!/bin/bash
+# aleo testnet3 激励测试一键部署脚本
+# 关注作者twitter   @simplefish3 ，不定期更新撸毛教程
+
+Workspace=/root/aleo
 ScreenName=aleo
-KeyFile="/aleo/aleo_key.txt"
+KeyFile="/root/my_aleo_key.txt"
 
 is_root() {
 	[[ $EUID != 0 ]] && echo -e "当前非root用户，请执行sudo su命令切换到root用户再继续执行(可能需要输入root用户密码)" && exit 1
 }
 
 # 判断screen是否已存在
-# 0 = 否   1 = 是
+# 0 = 是   1 = 否
 has_screen(){
 	Name=`screen -ls | grep ${ScreenName}`
 	if [ -z "${Name}" ]
 	then
-		return 0
-	else
-		echo "${Name}正在运行中..."
 		return 1
+	else
+		echo "screen 运行中：${Name}"
+		return 0
 	fi
 }
 
 # 进入screen环境
 go_into_screen(){
-	has_screen && echo "screen不存在，请先运行节点" && exit 1
 	screen -D -r ${ScreenName}
 
 }
@@ -35,8 +38,8 @@ kill_screen(){
 		exit 0
         else
 		ScreenPid=${Name%.*}
-		echo "强制关闭${Name}"
-		kill ScreenPid
+		echo "强制关闭screen: ${Name}"
+		kill ${ScreenPid}
 		echo "强制关闭完成"
         fi
 }
@@ -46,12 +49,18 @@ install_snarkos(){
 	# 判断是否为root用户
 	is_root
 
+	mkdir ${Workspace}
 	cd ${Workspace}	
 
 	echo "开始安装rust"
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh  -s -- -y
 	source $HOME/.cargo/env
 	echo "rust安装成功!"
+
+	echo "打开防火墙4133和3033端口"
+	sudo ufw allow 4133
+	sudo ufw allow 3033
+	echo "防火墙设置完毕"
 
 	echo "开始下载aleo代码"
 	git clone https://github.com/AleoHQ/snarkOS.git --depth 1 ${Workspace} 
@@ -72,10 +81,18 @@ install_snarkos(){
 	# 判断当前服务器是否已经有生成密钥了
 	if [ -f ${KeyFile} ] 
 	then 
-		echo "当前服务器已生成过密钥："
+		echo "当前服务器已生成过账户和密钥"
 	else
+		# 生成账户和密钥
 		snarkos account new > ${KeyFile}
 	fi
+
+	# 将密钥保存/etc/profile中使得可以被启动脚本读取
+	PrivateKey=$(cat ${KeyFile} | grep "Private Key" | awk '{print $3}')
+        echo "export PROVER_PRIVATE_KEY=$PrivateKey" >> /etc/profile
+        source /etc/profile
+
+	echo “账户和密钥保存在 ${KeyFile}，请妥善保管，以下是详细信息：”
 	cat ${KeyFile}
 }
 
@@ -87,7 +104,7 @@ run_client(){
 	cd ${Workspace}
 
 	# 判断是否已经有screen存在了
-	has_screen && echo "如果要重新启动，请先关闭已存在的screen(执行脚本命令6)" && exit 1
+	has_screen && echo "执行脚本命令5进入screen查看" && exit 1
 
 	# 启动一个screen,并在screen中启动client节点
 	screen -dmS ${ScreenName}	
@@ -106,7 +123,7 @@ run_prover(){
 	cd ${Workspace}
 
 	# 判断是否已经有screen存在了
-        has_screen && echo "如果要重新启动，请先关闭已存在的screen(执行脚本命令6)" && exit 1
+        has_screen && echo "执行脚本命令5进入screen查看" && exit 1
 
 	# 启动一个screen,并在screen中启动prover节点
         screen -dmS ${ScreenName}
@@ -117,21 +134,16 @@ run_prover(){
 	
 }
 
-# 进入screen查看运行情况
-get_in_screen(){
-	screen -D -r ${ScreenName}
-}
-
 
 
 echo && echo -e " 
-aleo testnet3 激励测试一键安装脚本
-欢迎关注作者 twitter  @simplefish3
+aleo testnet3 激励测试一键部署脚本
+关注作者 twitter  @simplefish3 , 不定期更新撸毛教程
  ———————————————————————
  1.安装 aleo
  2.运行 prover 节点
  3.运行 client 节点 (目前阶段暂时不需要运行client，可跳过)
- 4.读取 aleo 地址私钥
+ 4.查看 aleo 地址和私钥
  5.进入 screen 查看节点的运行情况，注意进入screen后，退出screen的命令是ctrl+A+D
  6.强制关闭 screen(使用kill的方式强制关闭screen，谨慎使用)
  ———————————————————————
